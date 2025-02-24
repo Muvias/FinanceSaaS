9031149f5498cb195673d64bee994cbb221a5f70
Vinicius Perazza <vi_guepardo@hotmail.com>
2025-02-24 16:03:53 -0300

    first commit

---
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,7 @@
+interface SignInPageProps {}
+
+export default function SignInPage({}: SignInPageProps) {
+    return (
+        <div>page</div>
+    )
+}
---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,7 @@
+interface SignUpPageProps {}
+
+export default function SignUpPage({}: SignUpPageProps) {
+    return (
+        <div>page</div>
+    )
+}
