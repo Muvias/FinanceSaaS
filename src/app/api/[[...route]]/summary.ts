da77a90ca63d36d8b7c8b8ce1200b9d258aa79ee
Vinicius Perazza <vi_guepardo@hotmail.com>
2025-05-16 22:29:04 -0300

    :feat: summary

--- src/app/api/[[...route]]/summary.ts
+++ src/app/api/[[...route]]/summary.ts
@@ -1,8 +1,78 @@
+import { and, eq, gte, lte, sql, sum } from "drizzle-orm";
 import { Hono } from "hono";
+import { z } from "zod";
+
+import { differenceInDays, parse, subDays } from "date-fns";
+
+import { db } from "@/db";
+import { accounts, transactions } from "@/db/schema";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { zValidator } from "@hono/zod-validator";
+import { calculatePercentageChange } from "@/lib/utils";
 
 const app = new Hono()
-    .get("/", async (c) => {
-        return c.json({ summary: true });
-    });
+    .get("/", clerkMiddleware(),
+        zValidator("query", z.object({
+            from: z.string().optional(),
+            to: z.string().optional(),
+            accountId: z.string().optional()
+        })),
+        async (c) => {
+            const auth = getAuth(c);
+            const { from, to, accountId } = c.req.valid("query");
+
+            if (!auth?.userId) return c.json({ error: "Unauthorized" }, 401);
+
+            const defaultTo = new Date();
+            const defaultFrom = subDays(defaultTo, 30);
+
+            const startDate = from ? parse(from, "yyy-MM-dd", new Date()) : defaultFrom;
+            const endDate = to ? parse(to, "yyy-MM-dd", new Date()) : defaultTo;
+
+            const periodLength = differenceInDays(endDate, startDate) + 1;
+            const lastPeriodStart = subDays(startDate, periodLength);
+            const lastPeriodEnd = subDays(endDate, periodLength);
+
+            async function fetchFinancialData(userId: string, startDate: Date, endDate: Date) {
+                return await db
+                    .select({
+                        income: sql`SUM(CASE WHEN ${transactions.amount} >= 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(Number),
+                        expenses: sql`SUM(CASE WHEN ${transactions.amount} < 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(Number),
+                        remaining: sum(transactions.amount).mapWith(Number),
+                    })
+                    .from(transactions)
+                    .innerJoin(accounts, eq(
+                        transactions.accountId,
+                        accounts.id
+                    ))
+                    .where(
+                        and(
+                            accountId ? eq(transactions.accountId, accountId) : undefined,
+                            eq(accounts.userId, userId),
+                            gte(transactions.date, startDate),
+                            lte(transactions.date, endDate),
+                        )
+                    );
+            };
+
+            const [currentPeriod] = await fetchFinancialData(
+                auth.userId,
+                startDate,
+                endDate
+            );
+
+            const [lastPeriod] = await fetchFinancialData(
+                auth.userId,
+                startDate,
+                endDate
+            );
+
+            const incomeChange = calculatePercentageChange(currentPeriod.income, lastPeriod.income);
+            const expensesChange = calculatePercentageChange(currentPeriod.expenses, lastPeriod.expenses);
+            const remainingChange = calculatePercentageChange(currentPeriod.remaining, lastPeriod.remaining);
+
+            return c.json({ currentPeriod, lastPeriod, incomeChange, expensesChange, remainingChange });
+        }
+    );
 
 export default app;
